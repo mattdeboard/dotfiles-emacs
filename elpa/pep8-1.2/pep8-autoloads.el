907d69305b0602cba8058871721133ff78889cf5
Matt DeBoard <matt.deboard@gmail.com>
2013-07-19 10:48:14 -0400

    Add pep8 mode.

---
+++ elpa/pep8-1.2/pep8-autoloads.el
@@ -0,0 +1,21 @@
+;;; pep8-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pep8" "pep8.el" (20969 20819 10124 537000))
+;;; Generated autoloads from pep8.el
+
+(autoload 'pep8 "pep8" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pep8-autoloads.el ends here
