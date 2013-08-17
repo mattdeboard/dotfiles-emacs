00cd0c3ff0d563188e299c72a6766c69cb48355e
Matt DeBoard <matt.deboard@gmail.com>
2013-08-17 17:35:35 -0400

    Add highlight-80+ and highlight-parents packages.

---
+++ elpa/highlight-80+-1.0/highlight-80+-autoloads.el
@@ -0,0 +1,22 @@
+;;; highlight-80+-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "highlight-80+" "highlight-80+.el" (21007 57058
+;;;;;;  102726 40000))
+;;; Generated autoloads from highlight-80+.el
+
+(autoload 'highlight-80+-mode "highlight-80+" "\
+Highlight the portions of lines longer than 80 characters.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; highlight-80+-autoloads.el ends here
---
+++ elpa/highlight-parentheses-20130523.1752/highlight-parentheses-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-parentheses" "20130523.1752" "highlight surrounding parentheses [github]" 'nil)
