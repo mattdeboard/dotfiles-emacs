fdf5bb62c25bf272ec4183a83c18b360f7e63b75
Matt DeBoard <matt.deboard@gmail.com>
2013-08-22 15:04:35 -0400

    Tweak flymake-coffee regex and add json mode.

---
+++ elpa/json-mode-0.1.2/json-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; json-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "json-mode" "json-mode.el" (21014 23972 799342
+;;;;;;  242000))
+;;; Generated autoloads from json-mode.el
+
+(autoload 'json-mode "json-mode" "\
+Major mode for editing JSON files
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; json-mode-autoloads.el ends here
