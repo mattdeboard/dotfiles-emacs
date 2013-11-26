306667ebecd0d11a2d73a55aa7128f54c689b089
Matt DeBoard <matt.deboard@gmail.com>
2013-11-26 17:10:20 -0500

    The requirements thing didn't pan out quite as well as I'd hoped.

---
+++ elpa/jedi-direx-20130525.1939/jedi-direx-autoloads.el
@@ -0,0 +1,27 @@
+;;; jedi-direx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "jedi-direx" "jedi-direx.el" (21141 7190 355898
+;;;;;;  70000))
+;;; Generated autoloads from jedi-direx.el
+
+(autoload 'jedi-direx:pop-to-buffer "jedi-direx" "\
+
+
+\(fn)" t nil)
+
+(autoload 'jedi-direx:switch-to-buffer "jedi-direx" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jedi-direx-autoloads.el ends here
